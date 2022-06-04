3819f90b04af75ce1ee272150fa07df599cb0653
rakiyama <ryojiro25@gmail.com>
2022-06-04 14:03:58 +0900

    コメント

--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,8 +1,17 @@
 // ローカル環境でスマートコントラクトのテストを行うためのプログラム
 
+// async/await, 逐次実行の実現のため
 const main = async () => {
+	// "WavePortal"コントラクトのコンパイル -> コントラクトに必要なファイルがartifactsディレクトリに生成される
+	// getContractFactory: デプロイライブラリのアドレスとWavePortalコントラクトを連携
+	// hre: hardhatが用意した全ての機能を含むオブジェクト, hardhat実行毎にHERにアクセスしているのでimport不要
 	const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+
+	// コントラクトのためだけにEthereumローカルネットワークを作成(スクリプト実行後破棄される)
+	// ローカルにマイナーを用意してブロックチェーンの構築
 	const waveContract = await waveContractFactory.deploy();
+
+	// コントラクトがローカルブロックチェーン上にデプロイされるまで待つ
 	const wavePortal = await waveContract.deployed();
   
 	console.log("WavePortal address: ", wavePortal.address);
