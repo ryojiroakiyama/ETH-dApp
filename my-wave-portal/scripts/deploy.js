02ecbd0335c49a4d8693ca85b6e1b10437bc7c41
rakiyama <ryojiro25@gmail.com>
2022-06-16 13:46:31 +0900

    eth付与のデプロイ

--- my-wave-portal/scripts/deploy.js
+++ my-wave-portal/scripts/deploy.js
@@ -1,28 +1,35 @@
 // smart contractとフロントエンドを結びつける役割をする
-// run.jsに比べて本番環境
+// run.jsはローカル環境にデプロイする用
+// deploy.jsは本番環境にデプロイする用
 
 const main = async () => {
-	const [deployer] = await hre.ethers.getSigners();
-	const accountBalance = await deployer.getBalance();
-	const waveContract = await hre.ethers.getContractFactory("WavePortal");
-	const wavePortal = await waveContract.deploy();
-  
-	// hardhat.config.jsを編集(metamaskの秘密鍵載せた)して, Rinkbyネットワークで実行したら
-	// アドレスはmetamaskのアカウントアドレスになってた
-	console.log("Deploying contracts with account: ", deployer.address);
-	console.log("Account balance: ", accountBalance.toString());
-	console.log("Contract deployed to: ", wavePortal.address);
-	console.log("Contract deployed by: ", deployer.address);
-  };
-  
-  const runMain = async () => {
-	try {
-	  await main();
-	  process.exit(0);
-	} catch (error) {
-	  console.error(error);
-	  process.exit(1);
-	}
-  };
-  
-  runMain();
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+  // hardhat.config.jsを編集(metamaskの秘密鍵載せた)して, Rinkbyネットワークで実行したら
+  // アドレスはmetamaskのアカウントアドレスになってた
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  /* コントラクトに資金提供してデプロイ */
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+
+  // 資金を追加するまでデプロイを待機
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
